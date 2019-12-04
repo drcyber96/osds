f69dcd2b07ee28dea35ae8a59d2c0ff32a5f1033
najmi hamdi <muhdnajmiey89@gmail.com>
2019-12-05 04:21:42 +0800

    test auto deploy

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,7 +3,7 @@
 <?php
 //welcome page english
 return[
-  'welcome'       => 'Welcome To iSCAM',
+  'welcome'       => 'Welcome To Online Scammer Database System',
    'Name'       => 'Name',
    'Trace Your Suspicious Scammers with One Single Search' => 'Trace Your Suspicious Scammers with One Single Search',
    'Search Now' => 'Search Now',
--- resources/lang/mly/messages.php
+++ resources/lang/mly/messages.php
@@ -2,7 +2,7 @@
 <?php
 //welcome page malay
 return[
-    'welcome'       => 'Selamat Datang Ke iSCAM',
+    'welcome'       => 'Selamat Datang Ke Sistem Pangkalan Data Scammer Atas Talian',
     'Name'       => 'Nama',
     'Trace Your Suspicious Scammers with One Single Search' => 'Mengesan Penipu yang Mencurigakan dengan Satu Carian Tunggal',
     'Search Now' => 'Cari Sekarang',

e07204b4e04537ec1dbc1bd17c45b4f87c31a160
najmi hamdi <muhdnajmiey89@gmail.com>
2019-12-05 22:22:33 +0800

    test auto deploy

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -32,7 +32,7 @@ body {
    <div class="row">
      <div class="col-lg-8 col-md-10 mx-auto">
        <h1 class="text-center text-black font-weight-bold">{{__('lang.iScam-Online Scammer Database System')}}</h1>
-       <p class="text-center text-align text-white font-weight-bold">{{__('lang.iScam is a median to find information related to online scammers who are actively involved fraud in selling and buying products in the sosical media and Ecommerce market.')}}</p>
+       <p class="text-center text-align text-white font-weight-bold ">{{__('lang.iScam is a median to find information related to online scammers who are actively involved fraud in selling and buying products in the sosical media and Ecommerce market.')}}</p>
 
      </div>
    </div>
@@ -45,6 +45,9 @@ body {
        <h1 class="text-black text-center font-weight-bold">{{__('lang.Contact Us')}}</h1>
        <p class="text-center text-align text-white font-weight-bold">{{__('lang.If there are any problem and question, you can get in touch with us at:-')}}</p>
         <p class="text-center text-align text-white font-weight-bold">iscam.osds@gmail.com</p>
+          <p class="text-center text-align  "> <a href="mailto:iscam.osds@gmail.com?Subject=Hello%20iSCAM" target="_top">Click here to send us an email!!!</a></p>
+
+
      </div>
    </div>
  </div>
